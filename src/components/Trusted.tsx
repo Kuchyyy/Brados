c8719bfd08cbba206b8e2a9d6c4409fa3a89b032
Kuchy <kacper.kuchcinsky@gmail.com>
2025-12-19 21:05:50 +0100

    poprawka 2

--- src/components/Trusted.tsx
+++ src/components/Trusted.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { InfiniteSlider } from "@/components/ui/infinite-slider";
 
 export default function InfiniteSliderBasic() {
@@ -34,24 +36,24 @@ export default function InfiniteSliderBasic() {
           WSZYTSKO W JEDNYM MIEJSCU
         </h2>
 
-        <div className="relative w-full h-[180px] overflow-hidden touch-pan-x">
-          <div className="absolute inset-0 mask-[linear-gradient(to_right,transparent_0%,black_15%,black_85%,transparent_100%)]">
+        <div className="relative w-full h-[180px] overflow-hidden touch-pan-y">
+          <div className="absolute inset-0 md:mask-[linear-gradient(to_right,transparent_0%,black_15%,black_85%,transparent_100%)]">
             <InfiniteSlider
               gap={16}
               reverse
-              className="w-full h-full"
-              duration={50}
+              duration={80}
+              className="w-full h-full transform-gpu will-change-transform"
             >
               {logos.map((logo, index) => (
                 <div
                   key={index}
-                  className="flex items-center justify-center w-[160px] h-[160px] bg-white rounded-md shadow-md/20 "
+                  className="flex items-center justify-center w-[160px] h-[160px] bg-white rounded-md md:shadow-md/20"
                 >
                   <img
                     src={logo.src}
                     alt={logo.alt}
                     draggable="false"
-                    className="max-w-[120px] max-h-[120px] object-contain  transition duration-300"
+                    className="max-w-[120px] max-h-[120px] object-contain"
                   />
                 </div>
               ))}
